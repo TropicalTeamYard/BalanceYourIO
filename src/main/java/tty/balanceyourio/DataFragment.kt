3904e53c3e8b6d1958646b7eaecdc9b8ddb34976
wcf <1533144693@qq.com>
2019-04-02 22:04:24 +0800

    更新了临时图标，切换页面效果和功能完成

---
+++ src/main/java/tty/balanceyourio/DataFragment.kt
@@ -0,0 +1,19 @@
+package tty.balanceyourio
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class DataFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_data, container, false)
+    }
+
+
+}
