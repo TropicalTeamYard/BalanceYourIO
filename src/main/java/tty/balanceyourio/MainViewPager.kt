7aa8822585d04959e89f169204d5a7c174d04d6b
wcf <1533144693@qq.com>
2019-04-04 22:21:29 +0800

    //

---
+++ src/main/java/tty/balanceyourio/MainViewPager.kt
@@ -0,0 +1,26 @@
+package tty.balanceyourio
+
+import android.content.Context
+import android.support.v4.view.ViewPager
+import android.util.AttributeSet
+import android.view.MotionEvent
+import java.util.jar.Attributes
+
+class MainViewPager:ViewPager{
+    constructor(context: Context):super(context)
+    constructor(context: Context, attributeSet: AttributeSet):super(context, attributeSet)
+
+    private var isScroll: Boolean = false
+
+    fun setScroll(scroll: Boolean) {
+        isScroll = scroll
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return this.isScroll && super.onTouchEvent(event)
+    }
+
+    override fun onInterceptTouchEvent(event: MotionEvent): Boolean {
+        return this.isScroll && super.onInterceptTouchEvent(event)
+    }
+}
