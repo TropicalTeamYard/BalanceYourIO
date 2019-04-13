21db50b200bb62bd56d84a975d29e1661fc6617d
wcf <1533144693@qq.com>
2019-04-14 00:33:45 +0800

    完成记账数字输入的逻辑，控制数值合法性

--- app/src/main/java/tty/balanceyourio/AddBillActivity.kt
+++ app/src/main/java/tty/balanceyourio/AddBillActivity.kt
@@ -1,33 +1,79 @@
 package tty.balanceyourio
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.AttributeSet
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
-import android.view.View
 import android.widget.RadioGroup
 import android.widget.SeekBar
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_add_bill.*
 import tty.util.AddBillRecyclerViewAdapter
 import tty.util.DataOperator
+import java.text.DecimalFormat
 import java.util.ArrayList
 import java.util.HashMap
 
-class AddBillActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener, SeekBar.OnSeekBarChangeListener {
-    var nowMoney=8
-    var nowProgress=8
-    var shouldChange=true
+class AddBillActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener, SeekBar.OnSeekBarChangeListener,
+    TextWatcher {
+    override fun afterTextChanged(s: Editable?) {
+
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        if(shouldInputMoneyChange){
+            val decimalFormat = DecimalFormat("0.00")
+            try {
+                if(s?.length!! >0&&s.toString().toDouble()>=0){
+                    nowMoney=s.toString().toDouble()
+                    nowMoney=decimalFormat.format(nowMoney).toDouble()
+                    add_show_now_money.text= "￥ $nowMoney"
+                } else if(s.isEmpty()) {
+                    nowMoney=0.0
+                    add_show_now_money.text= "￥ $nowMoney"
+                }
+                if(nowMoney>999999){
+                    nowMoney=999999.0
+                    add_show_now_money.text= "￥ $nowMoney"
+                    add_input_money.setText("999999")
+                    add_input_money.setSelection(add_input_money.text.length)
+                }
+            } catch (e : NumberFormatException) {
+                //e.printStackTrace()
+                if(nowMoney>0){
+                    add_show_now_money.text= "￥ $nowMoney"
+                } else {
+                    add_input_money.setText("")
+                }
+
+            }
+        } else {
+            shouldInputMoneyChange=true
+        }
+
+
+    }
+
+    var nowMoney=8.0
+    var nowProgress=8.0
+    private var shouldMoneyChange=true
+    private var shouldInputMoneyChange=true
     override fun onStopTrackingTouch(seekBar: SeekBar?) {
         //Log.d("ABA", "stop: "+seekBar?.progress)
-        shouldChange=false
+        shouldMoneyChange=false
+        shouldInputMoneyChange=false
         seekBar?.progress=8
-        nowProgress=8
+        nowProgress= 8.0
+        add_input_money.setText("")
 
     }
 
@@ -35,20 +81,25 @@ class AddBillActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener,
         //Log.d("ABA", "start: "+seekBar?.progress)
     }
 
-    @SuppressLint("SetTextI18n")
     override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
         //Log.d("ABA", "changed: "+seekBar?.progress)
-        if(shouldChange){
+        shouldInputMoneyChange=false
+        if(shouldMoneyChange){
             if(nowProgress<seekBar?.progress!!){
                 nowMoney++
             } else if(nowProgress> seekBar.progress) {
                 nowMoney--
             }
-            nowProgress= seekBar.progress
+            nowProgress= seekBar.progress.toDouble()
+            if(nowMoney<=0){
+                nowMoney= 0.0
+            } else if(nowMoney>999999){
+                nowMoney=999999.0
+            }
             Log.d("ABA", "now:$nowMoney")
-            add_show_now_money.text= "￥ ： $nowMoney"
+            add_show_now_money.text= "￥ $nowMoney"
         } else {
-            shouldChange=true
+            shouldMoneyChange=true
         }
 
     }
@@ -89,6 +140,8 @@ class AddBillActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener,
         recyclerView.adapter = adapter
         recyclerView.layoutManager = GridLayoutManager(this, 4)
         add_sb_money.setOnSeekBarChangeListener(this)
+        add_show_now_money.text="￥ $nowMoney"
+        add_input_money.addTextChangedListener(this)
     }
 
     companion object {
