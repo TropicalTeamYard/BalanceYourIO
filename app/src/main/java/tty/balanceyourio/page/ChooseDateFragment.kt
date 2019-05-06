a2d71f45792a425a0a64b6703f744c801760647e
wcf <1533144693@qq.com>
2019-05-06 12:41:55 +0800

    临时提交

---
+++ app/src/main/java/tty/balanceyourio/page/ChooseDateFragment.kt
@@ -0,0 +1,25 @@
+package tty.balanceyourio.page
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_choose_date.*
+import tty.balanceyourio.R
+
+class ChooseDateFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_choose_date, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        choose_date_calendar.date
+        //Toast.makeText()
+    }
+
+
+}
---
+++ app/src/main/java/tty/balanceyourio/util/DateConverter.kt
@@ -0,0 +1,4 @@
+package tty.balanceyourio.util
+
+class DateConverter {
+}
