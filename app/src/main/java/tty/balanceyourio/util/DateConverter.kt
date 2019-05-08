058b768a4c55d40ca4726010179e0246abc37cb7
wcf <1533144693@qq.com>
2019-05-08 18:02:58 +0800

    修复bug

--- app/src/main/java/tty/balanceyourio/util/DateConverter.kt
+++ app/src/main/java/tty/balanceyourio/util/DateConverter.kt
@@ -1,5 +1,6 @@
 package tty.balanceyourio.util
 
+import android.util.Log
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -16,12 +17,15 @@ object DateConverter {
     fun getSimpleString(d: Date): String{
         val calendar: Calendar= Calendar.getInstance()
         calendar.time=d
-        val h=calendar.get(Calendar.HOUR)
+        val h=calendar.get(Calendar.HOUR_OF_DAY)
+        Log.d("DC", "Hour: $h")
         return SimpleDateFormat("yyyy-MM-dd", Locale.CHINA).format(d)+when(h){
-            in 6..10->"上午"
-            in 11..12->"中午"
-            in 13..18->"下午"
-            else->"UNDEFINED"
+            in 6..10->" 上午"
+            in 11..12->" 中午"
+            in 13..18->" 下午"
+            in 19..24->" 晚上"
+            in 1..5->" 晚上"
+            else->" UNDEFINED"
         }
     }
 
