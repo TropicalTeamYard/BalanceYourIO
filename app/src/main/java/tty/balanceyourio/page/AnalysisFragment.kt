e3341ca357a64a16cfe52b788e92d2bbc79da051
wcf <1533144693@qq.com>
2019-05-25 03:17:54 +0800

    Update AnalysisFragment.kt

--- app/src/main/java/tty/balanceyourio/page/AnalysisFragment.kt
+++ app/src/main/java/tty/balanceyourio/page/AnalysisFragment.kt
@@ -1,6 +1,5 @@
 package tty.balanceyourio.page
 
-import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Build
 import android.os.Bundle
@@ -12,11 +11,11 @@ import android.view.View
 import android.view.ViewGroup
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.formatter.IFillFormatter
+import com.github.mikephil.charting.formatter.ValueFormatter
 import kotlinx.android.synthetic.main.fragment_analysis.*
 import tty.balanceyourio.R
 import tty.balanceyourio.data.BYIOHelper
@@ -57,30 +56,55 @@ class AnalysisFragment : Fragment() {
 //        desc.text="按日统计"
 //        chart.description = desc
         chart.description.isEnabled=false
-        chart.setBackgroundColor(resources.getColor(R.color.colorPrimary, null))
+//        chart.setBackgroundColor(resources.getColor(R.color.colorAccent, null))
         chart.setNoDataText("暂无数据")
         chart.setTouchEnabled(true)
         chart.isDoubleTapToZoomEnabled=false
         chart.isDragEnabled = true
-        chart.isScaleXEnabled = false
+        chart.isScaleXEnabled = true
         chart.isScaleYEnabled = false
         chart.setPinchZoom(false)
-        val x = chart.xAxis
-        x.setLabelCount(7, false)
-        x.textColor = Color.WHITE
-        x.position = XAxis.XAxisPosition.BOTTOM_INSIDE
-        x.setDrawGridLines(false)
-        x.axisLineColor = Color.WHITE
 
-        val y = chart.axisLeft
-        y.setLabelCount(6, false)
-        y.textColor = Color.WHITE
-        y.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART)
-        y.setDrawGridLines(false)
-        y.axisLineColor = Color.WHITE
-        chart.axisRight.isEnabled = false
+        val x = chart.xAxis
+        x.labelCount=7
+        x.setAvoidFirstLastClipping(true)
+        x.granularity=1F
+        //设置一页最大显示个数为7
+        val ratio = statisticsList.size.toFloat() / 7
+        //显示的时候是按照多大的比率缩放显示,1F表示不放大缩小
+        chart.zoom(ratio,1F,0F,0F)
+        x.isGranularityEnabled=true
+        x.textColor = resources.getColor(R.color.colorNormal, null)
+        x.position = XAxis.XAxisPosition.BOTTOM
+        x.setDrawGridLines(false)
+        x.axisLineColor = resources.getColor(R.color.colorNormal, null)
+
+        val yOutcome = chart.axisLeft
+        yOutcome.setLabelCount(7, false)
+        yOutcome.axisLineWidth=1F
+        yOutcome.textColor = resources.getColor(R.color.typeOutcome, null)
+        yOutcome.setDrawGridLines(false)
+        yOutcome.axisLineColor = resources.getColor(R.color.typeOutcome, null)
+
+        val yIncome = chart.axisRight
+        yIncome.setLabelCount(7, false)
+        yIncome.axisLineWidth=1F
+        yIncome.textColor = resources.getColor(R.color.typeIncome, null)
+        yIncome.setDrawGridLines(false)
+        yIncome.axisLineColor = resources.getColor(R.color.typeIncome, null)
 
         setData(chart, statisticsList, timeMode)
+
+        x.valueFormatter=object : ValueFormatter(){
+            override fun getFormattedValue(value: Float): String {
+                val p=value.toInt()
+                return if(p>0 && p<statisticsList.size){
+                    "Pos.$p"
+                } else {
+                    ""
+                }
+            }
+        }
 
         chart.invalidate()
 
@@ -96,41 +120,60 @@ class AnalysisFragment : Fragment() {
     @RequiresApi(Build.VERSION_CODES.M)
     private fun setData(chart: LineChart, statistics: ArrayList<HashMap<IOType, Double>>, timeMode: TimeMode) {
 
-        val values = java.util.ArrayList<Entry>()
+        val valuesOutcome = java.util.ArrayList<Entry>()
+        val valuesIncome = java.util.ArrayList<Entry>()
 
         for (i in 0 until statistics.size) {
-            val `val` = statistics[i][IOType.Outcome]!!.toFloat()
-            values.add(Entry(i.toFloat(), `val`))
+            valuesOutcome.add(Entry(i.toFloat(), statistics[i][IOType.Outcome]!!.toFloat()))
+            valuesIncome.add(Entry(i.toFloat(), statistics[i][IOType.Income]!!.toFloat()))
         }
 
-        val set1: LineDataSet
-
+        val outcomeSet: LineDataSet
+        val incomeSet: LineDataSet
         if (chart.data != null && chart.data.dataSetCount > 0) {
-            set1 = chart.data.getDataSetByIndex(0) as LineDataSet
-            set1.values = values
+            outcomeSet = chart.data.getDataSetByIndex(0) as LineDataSet
+            outcomeSet.values = valuesOutcome
+            incomeSet = chart.data.getDataSetByIndex(1) as LineDataSet
+            incomeSet.values = valuesOutcome
             chart.data.notifyDataChanged()
             chart.notifyDataSetChanged()
         } else {
-            set1 = LineDataSet(values, "支出")
-            set1.mode = LineDataSet.Mode.CUBIC_BEZIER
-            set1.cubicIntensity = 0.2f
-            set1.lineWidth = 1.8f
-            set1.circleRadius = 2.4f
-            set1.setCircleColor(Color.WHITE)
-            set1.highLightColor = Color.rgb(244, 117, 117)
-            set1.color = Color.WHITE
-            set1.fillColor = resources.getColor(R.color.colorAccent, null)
-            set1.fillAlpha = 100
-            set1.setDrawHorizontalHighlightIndicator(true)
-            set1.fillFormatter = IFillFormatter { dataSet, dataProvider -> chart.axisLeft.axisMinimum }
-            set1.setDrawFilled(true)
-            set1.setDrawValues(true)
+            outcomeSet = LineDataSet(valuesOutcome, "支出")
+            outcomeSet.mode = LineDataSet.Mode.CUBIC_BEZIER
+            outcomeSet.cubicIntensity = 0.2f
+            outcomeSet.lineWidth = 1.8f
+            outcomeSet.circleRadius = 2.4f
+            outcomeSet.setCircleColor(resources.getColor(R.color.typeOutcome, null))
+            outcomeSet.highLightColor = resources.getColor(R.color.typeOutcome, null)
+            outcomeSet.color = resources.getColor(R.color.typeOutcome, null)
+            outcomeSet.fillColor = resources.getColor(R.color.typeOutcome, null)
+            outcomeSet.fillAlpha = 100
+            outcomeSet.setDrawHorizontalHighlightIndicator(true)
+            outcomeSet.fillFormatter = IFillFormatter { dataSet, dataProvider -> chart.axisLeft.axisMinimum }
+            outcomeSet.setDrawFilled(true)
+            outcomeSet.setDrawValues(true)
+            outcomeSet.valueTextColor = resources.getColor(R.color.typeOutcome, null)
 
-            // create a data object with the data sets
-            val data = LineData(set1)
+            incomeSet = LineDataSet(valuesIncome, "收入")
+            incomeSet.mode = LineDataSet.Mode.CUBIC_BEZIER
+            incomeSet.cubicIntensity = 0.2f
+            incomeSet.lineWidth = 1.8f
+            incomeSet.circleRadius = 2.4f
+            incomeSet.setCircleColor(resources.getColor(R.color.typeIncome, null))
+            incomeSet.highLightColor = resources.getColor(R.color.typeIncome, null)
+            incomeSet.color = resources.getColor(R.color.typeIncome, null)
+            incomeSet.fillColor = resources.getColor(R.color.typeIncome, null)
+            incomeSet.fillAlpha = 100
+            incomeSet.setDrawHorizontalHighlightIndicator(true)
+//            incomeSet.axisDependency = YAxis.AxisDependency.RIGHT
+            incomeSet.fillFormatter = IFillFormatter { dataSet, dataProvider -> chart.axisRight.axisMinimum }
+            incomeSet.setDrawFilled(true)
+            incomeSet.setDrawValues(true)
+            incomeSet.valueTextColor = resources.getColor(R.color.typeIncome, null)
+
+            val data = LineData(outcomeSet, incomeSet)
             data.setValueTypeface(tfLight)
             data.setValueTextSize(9f)
-            data.setValueTextColor(resources.getColor(R.color.typeOutcome, null))
 
             chart.data = data
         }
