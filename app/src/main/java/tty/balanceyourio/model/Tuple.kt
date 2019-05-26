28b774a0c23f45ab1e5a1bedd03819f0ca266cb1
h1542462994 <t1542462994@outlook.com>
2019-05-26 13:53:47 +0800

    Create Tuple.kt

---
+++ app/src/main/java/tty/balanceyourio/model/Tuple.kt
@@ -0,0 +1,11 @@
+package tty.balanceyourio.model
+
+class Tuple<T1,T2,T3>(item1: T1, item2: T2, item3: T3) {
+
+    var item1:T1 = item1
+        private  set
+    var item2:T2 = item2
+        private  set
+    var item3:T3 = item3
+        private set
+}
